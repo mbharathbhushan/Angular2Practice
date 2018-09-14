307feb78328a7f3b25a5d2f748070c6133f87e77
Bharath Bhushan M <mbharath1983@gmail.com>
2018-09-14 19:47:49 +0530

    Angular 02 - 09 - Property Binding in Angular 02
    Angular 02 - 09 - Property Binding in Angular 02

---
+++ 09 - Property Binding In Angular 2/09 - Property Binding In Angular 2/src/app/employee/employee.component.ts
@@ -0,0 +1,13 @@
+﻿import { Component } from '@angular/core'
+@Component(
+    {
+        selector: 'my-employee',
+        templateUrl: 'app/employee/employee.component.html',
+        styleUrls: ['app/employee/employee.component.css']
+    })
+export class EmployeeComponent {
+    firstName: string = 'Bharath';
+    lastName: string = 'Mangalpally';
+    gender: string = 'male';
+    age: number = 20;
+}

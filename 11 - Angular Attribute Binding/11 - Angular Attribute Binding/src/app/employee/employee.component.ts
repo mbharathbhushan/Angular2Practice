501c8116993fd52ecd2788eddf6737114424e0f7
M.Bharath Bhushan <mbharath278@hotmail.com>
2018-09-19 12:02:14 +0530

    Angualar 02  11  Angular Attribute Binding - Part 1
    Angualar 02  11  Angular Attribute Binding - Part 1

---
+++ 11 - Angular Attribute Binding/11 - Angular Attribute Binding/src/app/employee/employee.component.ts
@@ -0,0 +1,12 @@
+﻿import { Component } from '@angular/core'
+@Component(
+    {
+        selector: 'my-employee',
+        templateUrl: 'app/employee/employee.component.html'
+    })
+export class EmployeeComponent {
+    firstName: string = 'Bharath';
+    lastName: string = 'Mangalpally';
+    gender: string = 'male';
+    age: number = 20;
+}

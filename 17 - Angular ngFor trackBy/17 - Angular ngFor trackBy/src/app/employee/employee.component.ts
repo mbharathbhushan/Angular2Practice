b38d112d5c82fe80ffad3b9d7a880605bd4523fa
Bharath Bhushan M <mbharath1983@gmail.com>
2018-11-28 19:50:08 +0530

    17 - Angular ngFor trackBy
    17 - Angular ngFor trackBy

---
+++ 17 - Angular ngFor trackBy/17 - Angular ngFor trackBy/src/app/employee/employee.component.ts
@@ -0,0 +1,20 @@
+﻿import { Component } from '@angular/core'
+@Component(
+    {
+        selector: 'my-employee',
+        templateUrl: 'app/employee/employee.component.html',
+        styleUrls: ['app/employee/employee.component.css']
+    })
+export class EmployeeComponent {
+    columnSpan: number = 2;
+    firstName: string = 'Bharath';
+    lastName: string = 'Mangalpally';
+    gender: string = 'male';
+    age: number = 20;
+    showDetails: boolean = false;
+
+    toggleDetails(): void {
+        this.showDetails = !this.showDetails
+    }
+
+}

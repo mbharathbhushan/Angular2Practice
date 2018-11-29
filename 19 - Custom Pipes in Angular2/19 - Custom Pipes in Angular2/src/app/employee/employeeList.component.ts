016dbb5439b2269d7721d4375a39b0b620ea314d
Bharath Bhushan M <mbharath1983@gmail.com>
2018-11-29 11:36:09 +0530

    19 - Custom Pipes In Angular 2 - Part 2
    19 - Custom Pipes In Angular 2 - Part 2

---
+++ 19 - Custom Pipes in Angular2/19 - Custom Pipes in Angular2/src/app/employee/employeeList.component.ts
@@ -0,0 +1,63 @@
+﻿import { Component } from '@angular/core'
+@Component({
+    selector: 'list-employee',
+    templateUrl: 'app/employee/employeeList.component.html',
+    styleUrls: ['app/employee/employeeList.component.css']
+})
+
+export class EmployeeListComponent {
+    employees: any[];
+
+    constructor() {
+        this.employees = [
+            {
+                code: 'emp101', name: 'Tom', gender: 'Male',
+                annualSalary: 5500, dateOfBirth: '06/25/1988'
+            },
+            {
+                code: 'emp102', name: 'Alex', gender: 'Male',
+                annualSalary: 5700.95, dateOfBirth: '9/6/1982'
+            },
+            {
+                code: 'emp103', name: 'Mike', gender: 'Male',
+                annualSalary: 5900, dateOfBirth: '12/8/1979'
+            },
+            {
+                code: 'emp104', name: 'Mary', gender: 'Female',
+                annualSalary: 6500.826, dateOfBirth: '10/14/1980'
+            },
+        ];
+    }
+
+    refreshEmployees(): void {
+        this.employees = [
+            {
+                code: 'emp101', name: 'Tom', gender: 'Male',
+                annualSalary: 5500, dateOfBirth: '6/25/1988'
+            },
+            {
+                code: 'emp102', name: 'Alex', gender: 'Male',
+                annualSalary: 5700.95, dateOfBirth: '9/6/1982'
+            },
+            {
+                code: 'emp103', name: 'Mike', gender: 'Male',
+                annualSalary: 5900, dateOfBirth: '12/8/1979'
+            },
+            {
+                code: 'emp104', name: 'Mary', gender: 'Female',
+                annualSalary: 6500.826, dateOfBirth: '10/14/1980'
+            },
+            {
+                code: 'emp105', name: 'Maanvi', gender: 'Female',
+                annualSalary: 6500.826, dateOfBirth: '10/14/1981'
+            }
+        ];
+    };
+
+    trackByCode(index: number, empl: any): string {
+        return empl.code;
+    }
+
+
+
+}

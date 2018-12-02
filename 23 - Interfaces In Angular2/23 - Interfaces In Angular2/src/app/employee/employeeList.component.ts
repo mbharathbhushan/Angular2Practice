647b6a92f96771d97bc6878874ceb8646c521094
M.Bharath Bhushan <mbharath278@hotmail.com>
2018-12-02 09:34:38 +0530

    23 - Interfaces In Angular 2
    23 - Interfaces In Angular 2

---
+++ 23 - Interfaces In Angular2/23 - Interfaces In Angular2/src/app/employee/employeeList.component.ts
@@ -0,0 +1,81 @@
+﻿import { Component } from '@angular/core'
+import { IEmployee } from './employee';
+
+@Component({
+    selector: 'list-employee',
+    templateUrl: 'app/employee/employeeList.component.html',
+    styleUrls: ['app/employee/employeeList.component.css']
+})
+
+export class EmployeeListComponent {
+    employees: IEmployee[];
+
+    selectedRadioButtonValue: string = "All";
+
+    onEmployeeCountRadioButtonChanged(selectedValue: string) {
+        this.selectedRadioButtonValue = selectedValue;
+    }
+
+    constructor() {
+        this.employees = [
+            {
+                code: 'emp101', name: 'Tom', gender: 'Male',
+                annualSalary: 5500, dateOfBirth: '06/25/1988'
+            },
+            {
+                code: 'emp102', name: 'Alex', gender: 'Male',
+                annualSalary: 5700.95, dateOfBirth: '9/6/1982'
+            },
+            {
+                code: 'emp103', name: 'Mike', gender: 'Male',
+                annualSalary: 5900, dateOfBirth: '12/8/1979'
+            },
+            {
+                code: 'emp104', name: 'Mary', gender: 'Female',
+                annualSalary: 6500.826, dateOfBirth: '10/14/1980'
+            },
+        ];
+    }
+
+    refreshEmployees(): void {
+        this.employees = [
+            {
+                code: 'emp101', name: 'Tom', gender: 'Male',
+                annualSalary: 5500, dateOfBirth: '6/25/1988'
+            },
+            {
+                code: 'emp102', name: 'Alex', gender: 'Male',
+                annualSalary: 5700.95, dateOfBirth: '9/6/1982'
+            },
+            {
+                code: 'emp103', name: 'Mike', gender: 'Male',
+                annualSalary: 5900, dateOfBirth: '12/8/1979'
+            },
+            {
+                code: 'emp104', name: 'Mary', gender: 'Female',
+                annualSalary: 6500.826, dateOfBirth: '10/14/1980'
+            },
+            {
+                code: 'emp105', name: 'Maanvi', gender: 'Female',
+                annualSalary: 6500.826, dateOfBirth: '10/14/1981'
+            }
+        ];
+    };
+
+    trackByCode(index: number, empl: any): string {
+        return empl.code;
+    }
+
+    getTotalEmployeesCount(): number {
+        return this.employees.length;
+    }
+
+    getTotalMaleEmployeesCount(): number {
+        return this.employees.filter(e =>e.gender === 'Male').length;
+    }
+
+    getTotalFemaleEmployeesCount(): number {
+        return this.employees.filter(e => e.gender === 'Female').length;
+    }
+
+}

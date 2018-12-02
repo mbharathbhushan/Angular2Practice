cfc6b398feef2c9bebcb142b58d54f0fad07a4cb
M.Bharath Bhushan <mbharath278@hotmail.com>
2018-12-02 13:06:01 +0530

    25 - Service in Angular2
    25 - Service in Angular2

---
+++ 25 - Service In Angular2/25 - Service In Angular2/src/app/employee/employee.ts
@@ -0,0 +1,18 @@
+﻿export interface IEmployee {
+    code: string;
+    name: string;
+    gender: string;
+    annualSalary: number;
+    dateOfBirth: string;
+    ////department?: string;
+
+    ////computeMonthlySalary(annualSalary: number): number;
+}
+
+export class Employee implements IEmployee {
+    ////computeMonthlySalary(annualSalary: number): number {
+    ////    return annualSalary / 12;
+    ////}
+    constructor(public code: string, public name: string, public gender: string, public annualSalary: number, public dateOfBirth: string) {
+    }
+}
